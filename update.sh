5692e7df23991300102f2e81ac9c10e9a6343387
Brian Savage <savage13@gmail.com>
2025-01-26 11:11:25 -0500

    Add Links from route to editor

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES="*.png *.svg *.html *.js *.json *.celer *.css leaflet codemirror node_modules"
+FILES="*.png *.svg *.html *.mjs *.js *.json *.celer *.css leaflet codemirror node_modules"
 
 rsync -arvz $FILES restite.org:html/notceler-totk
 
